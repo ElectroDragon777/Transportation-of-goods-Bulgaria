578c07704482f99d6cfe9bbad74b84acbf51fb22
ElectroDragon <ElectroDragon777@users.noreply.github.com>
2025-04-14 20:21:01 +0300

    Map fix :D (Refer to https://leafletjs.com/examples/quick-start/)

--- App/Views/CourierTracking/index.php
+++ App/Views/CourierTracking/index.php
@@ -121,7 +121,7 @@
                                             <!-- Load Leaflet CSS -->
                                             <<link rel="stylesheet"
                                                 href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-                                                integrity="sha256-gejK7ejef6bhliuqigU0J0brnx9tJwbreKz/Au5tsSI="
+                                                integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
                                                 crossorigin="" />
 
                                             <!-- Map container -->
@@ -135,7 +135,7 @@
 
                                             <!-- Load Leaflet JS -->
                                             <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-                                                integrity="sha256-20nQHpuErihq6mgcVyvUs3jtsjbj6LWrwhUb7G5czt2M="
+                                                integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
                                                 crossorigin=""></script>
 
                                             <!-- Load our custom map script AFTER Leaflet -->
