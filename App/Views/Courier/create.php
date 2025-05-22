5698b5bd1b1799bff7dc5e0c69e08654276e4f25
ElectroDragon <ElectroDragon777@users.noreply.github.com>
2025-05-22 19:32:25 +0300

    Enhanced Creation profiles to be like Register

--- App/Views/Courier/create.php
+++ App/Views/Courier/create.php
@@ -13,20 +13,19 @@
                         action="<?php echo INSTALL_URL; ?>?controller=Courier&action=create">
                         <input type="hidden" name="send" value="1" />
 
+                        <h5 class="with-line">Their Name (Username) and Email:</h5>
                         <div class="row">
                             <div class="form-group col-md-6 mb-3">
                                 <label for="name" class="form-label">Name*</label>
                                 <input type="text" class="form-control" id="name" name="name" required>
                             </div>
-                        </div>
-
-                        <div class="row">
                             <div class="form-group col-md-6 mb-3">
                                 <label for="email" class="form-label">Email*</label>
                                 <input type="email" class="form-control" id="email" name="email" required>
                             </div>
                         </div>
 
+                        <h5 class="with-line">Their Password:</h5>
                         <div class="row">
                             <div class="form-group col-md-6 mb-3">
                                 <label for="password" class="form-label">Password*</label>
@@ -37,14 +36,6 @@
                                 </div>
                             </div>
                             <div class="form-group col-md-6 mb-3">
-                                <label for="phoneNumber" class="form-label">Phone Number</label>
-                                <input type="tel" pattern="^\d{10}$" class="form-control" id="phoneNumber"
-                                    name="phone_number" required>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="form-group col-md-6 mb-3">
                                 <label for="repeatPassword" class="form-label">Repeat Password*</label>
                                 <div class="position-relative">
                                     <input type="password" class="form-control pe-5" id="repeatPassword"
@@ -52,6 +43,26 @@
                                     <i class="password-toggle-icon fa fa-eye" data-target="repeatPassword"></i>
                                 </div>
                             </div>
+                        </div>
+
+                        <h5 class="with-line">Their Phone number:</h5>
+                        <div class="row">
+                            <div class="form-group col-md-6 mb-3">
+                                <label for="phoneNumber" class="form-label">Phone Number*</label>
+                                <input type="tel" pattern="^\d{10}$" class="form-control" id="phoneNumber"
+                                    name="phone_number" required>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="text-muted mt-3">
+                                <strong>Please note:</strong><br>
+                                <span class="note-indicator"></span>
+                                * All fields are required.<br>
+                                <span class="note-indicator"></span>
+                                The phone number is required to be in the format of 10 digits (e.g., 1234567890).
+                                Also, it is required for special cases.<br>
+
+                            </p>
                         </div>
                         <div class="row">
                             <div class="col-12">
@@ -66,3 +77,20 @@
         </div>
     </div>
 </div>
+
+<style>
+    .note-indicator {
+        display: inline-block;
+        /* Allows us to control width and height */
+        width: 20px;
+        /* Adjust the length of the line */
+        height: 2px;
+        /* Adjust the thickness of the line */
+        background-color: #007bff;
+        /* Choose your desired color (Bootstrap primary color used as an example) */
+        vertical-align: middle;
+        /* Align the line with the text */
+        margin-right: 8px;
+        /* Add some spacing between the line and the text */
+    }
+</style>
--- App/Views/User/create.php
+++ App/Views/User/create.php
@@ -13,20 +13,20 @@
                         action="<?php echo INSTALL_URL; ?>?controller=User&action=create">
                         <input type="hidden" name="send" value="1" />
 
+                        <h5 class="with-line">Generic information:</h5>
                         <div class="row">
                             <div class="form-group col-md-6 mb-3">
                                 <label for="name" class="form-label">Name*</label>
                                 <input type="text" class="form-control" id="name" name="name" required>
                             </div>
-                        </div>
 
-                        <div class="row">
                             <div class="form-group col-md-6 mb-3">
                                 <label for="email" class="form-label">Email*</label>
                                 <input type="email" class="form-control" id="email" name="email" required>
                             </div>
                         </div>
 
+                        <h5 class="with-line">Get yourself a password:</h5>
                         <div class="row">
                             <div class="form-group col-md-6 mb-3">
                                 <label for="password" class="form-label">Password*</label>
@@ -37,14 +37,6 @@
                                 </div>
                             </div>
                             <div class="form-group col-md-6 mb-3">
-                                <label for="phoneNumber" class="form-label">Phone Number</label>
-                                <input type="tel" pattern="^\d{10}$" class="form-control" id="phoneNumber"
-                                    name="phone_number">
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="form-group col-md-6 mb-3">
                                 <label for="repeatPassword" class="form-label">Repeat Password*</label>
                                 <div class="position-relative">
                                     <input type="password" class="form-control pe-5" id="repeatPassword"
@@ -52,6 +44,25 @@
                                     <i class="password-toggle-icon fa fa-eye" data-target="repeatPassword"></i>
                                 </div>
                             </div>
+                        </div>
+
+                        <h5 class="with-line">Phone number (not required):</h5>
+                        <div class="row">
+                            <div class="form-group col-md-6 mb-3">
+                                <label for="phoneNumber" class="form-label">Phone Number</label>
+                                <input type="tel" pattern="^\d{10}$" class="form-control" id="phoneNumber"
+                                    name="phone_number">
+                            </div>
+                        </div>
+
+                        <div>
+                            <p class="text-muted mt-3">
+                                <strong>Please note:</strong><br>
+                                <span class="note-indicator"></span>
+                                * Field is required.<br>
+                                <span class="note-indicator"></span>
+                                The phone number is required to be in the format of 10 digits (e.g., 1234567890).<br>
+                            </p>
                         </div>
 
                         <div class="row">
@@ -67,3 +78,20 @@
         </div>
     </div>
 </div>
+
+<style>
+    .note-indicator {
+        display: inline-block;
+        /* Allows us to control width and height */
+        width: 20px;
+        /* Adjust the length of the line */
+        height: 2px;
+        /* Adjust the thickness of the line */
+        background-color: #007bff;
+        /* Choose your desired color (Bootstrap primary color used as an example) */
+        vertical-align: middle;
+        /* Align the line with the text */
+        margin-right: 8px;
+        /* Add some spacing between the line and the text */
+    }
+</style>
